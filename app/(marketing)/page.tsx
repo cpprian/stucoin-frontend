b3bc7a78dec3b121d69e95e97d39271968298b54
cpprian <cpprian456@gmail.com>
2023-12-25 21:05:30 +0100

    init shadcn & add first components with basic dir structure

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { Button } from '@/components/ui/button'
+import { ToastAction } from '@/components/ui/toast'
+import { useToast } from '@/components/ui/use-toast'
+
+export default function Home() {
+  const { toast } = useToast() 
+
+  return (
+    <Button
+      onClick={() => {
+        toast({
+          variant: "default",
+          title: "Uh oh! Something went wrong.",
+          description: "There was a problem with your request.",
+          action: (
+            <ToastAction altText='Try again'>Try again</ToastAction>
+          ),
+        })
+      }}
+    >
+      Show Toast
+    </Button>
+  )
+}
