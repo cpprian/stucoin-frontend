2f51582a8c5aee3aa9138315078567cbbd4eca6d
cpprian <cpprian456@gmail.com>
2024-01-06 14:36:32 +0100

    upload css,img to github pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     "output": "export",
+    reactStrictMode: true,
+    basePath: "https://github.com/cpprian/stucoin-frontend"
 }
 
 module.exports = nextConfig
