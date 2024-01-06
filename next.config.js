73870e01027f28f992dbecc97841eaa9cc277e55
cpprian <cpprian456@gmail.com>
2024-01-06 15:38:31 +0100

    git ignorecase false

--- next.config.js
+++ next.config.js
@@ -1,19 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    "output": "export",
-    reactStrictMode: true,
-    basePath: "",
-    assetPrefix: "/stucoin-frontend/",
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'cpprian.github.io',
-            port: '',
-            pathname: '/stucoin-frontend/public',
-          },
-        ],
-      },
+    // "output": "export",
+    // reactStrictMode: true,
+    // basePath: "",
+    // assetPrefix: "/stucoin-frontend/",
 }
 
 module.exports = nextConfig
