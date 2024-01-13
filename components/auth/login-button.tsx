6d6c2c6373edd3667a3ccc4d5cc02352ef28a193
cpprian <cpprian456@gmail.com>
2024-01-13 19:31:50 +0100

    simplify LoginButton

--- components/auth/login-button.tsx
+++ components/auth/login-button.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-
 import {
     Dialog,
     DialogContent,
@@ -17,31 +15,17 @@ interface LoginButtonProps {
 
 export const LoginButton = ({
     children,
-    mode = "redirect",
     asChild,
 }: LoginButtonProps) => {
-    const router = useRouter();
-
-    const onClick = () => {
-        router.push("/auth/login");
-    };
-
-    if (mode === "modal") {
-        return (
-            <Dialog>
-                <DialogTrigger asChild={asChild}>
-                    {children}
-                </DialogTrigger>
-                <DialogContent className="p-0 w-auto bg-transparent border-none">
-                    <LoginForm />
-                </DialogContent>
-            </Dialog>
-        );
-    }
 
     return (
-        <span onClick={onClick} className="cursor-pointer">
-            {children}
-        </span>
+        <Dialog>
+            <DialogTrigger asChild={asChild}>
+                {children}
+            </DialogTrigger>
+            <DialogContent className="p-0 w-auto bg-transparent border-none">
+                <LoginForm />
+            </DialogContent>
+        </Dialog>
     );
 };
