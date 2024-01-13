1f8e2e9ead90afeb09b3d6c50f449c1ab5b9a7b8
cpprian <cpprian456@gmail.com>
2024-01-13 19:08:19 +0100

    add LoginButton

--- app/(public)/_components/navbar.tsx
+++ app/(public)/_components/navbar.tsx
@@ -2,13 +2,19 @@
 
 import { ModeToggle } from "@/components/mode-toggle";
 import { Logo } from "./logo";
+import { LoginButton } from "@/components/auth/login-button";
+import { Button } from "@/components/ui/button";
 
 export const Navbar = () => {
     return (
         <div className="z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6">
             <Logo />
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-4">
-                 {/* TODO: add login button */}
+                <LoginButton asChild>
+                    <Button variant="outline">
+                        Sign In
+                    </Button>
+                </LoginButton>
                 <ModeToggle />
             </div>
         </div>
