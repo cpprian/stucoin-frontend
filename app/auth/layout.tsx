c3084c2b8ce0cc21d6c5a231f0bb6a05b8006488
cpprian <cpprian456@gmail.com>
2024-01-13 23:58:13 +0100

    add UserButton & LogoutButton

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -6,7 +6,7 @@ const AuthLayout = ({
     children: React.ReactNode
 }) => {
     return (
-        <div className="h-full flex items-center justify-center">
+        <div className="h-full flex justify-center">
             <Navbar />
             {children}
         </div>
