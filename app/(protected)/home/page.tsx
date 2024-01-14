ab5c8c6356c7e6730c73fab092a15daaba6f2717
cpprian <cpprian456@gmail.com>
2024-01-14 23:27:07 +0100

    delete division based on role

---
+++ app/(protected)/home/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { UserButton } from "@/components/auth/user-button";
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <UserButton />
+        </div>
+    );
+}
+
+export default HomePage;
