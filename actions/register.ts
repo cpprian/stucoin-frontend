cce2861a954a6194c00ed0dc861b7311dc9dea32
cpprian <cpprian456@gmail.com>
2024-01-14 23:24:25 +0100

    add redirect to reigister action

--- actions/register.ts
+++ actions/register.ts
@@ -5,6 +5,9 @@ import { z } from "zod";
 import bcrypt from "bcryptjs";    
 import { getUserByEmail } from "@/data/user";
 import { db } from "@/lib/db";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
@@ -32,5 +35,22 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
         },
     });
 
-    return { success: "User created!" };
+    try {
+        await signIn("credentials", {
+            email,
+            password,
+            redirectTo: DEFAULT_LOGIN_REDIRECT,
+        })
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case "CredentialsSignin":
+                    return { error: "Invalid credentials!" }
+                default:
+                    return { error: "Something went wrong!" }
+            }
+        }
+
+        throw error;
+    }
 };
