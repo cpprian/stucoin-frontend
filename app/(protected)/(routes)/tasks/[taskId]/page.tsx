ae3dddb569b038cb76ac698c1b30167ec8103a8e
cpprian <cpprian456@gmail.com>
2024-01-15 15:49:04 +0100

    fix vercel issue

---
+++ app/(protected)/(routes)/tasks/[taskId]/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+import Editor from "@/components/editor";
+import { Cover } from "@/components/cover";
+import { Toolbar } from "@/components/toolbar";
+
+
+const HomePage = () => {
+    const document = "";
+
+    if (document === undefined) {
+        return (
+            <div>
+                <Cover.Skeleton />
+                <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
+                    <div className="space-y-4 pl-8 pt-4">
+                        <Skeleton className="h-14 w-[50%]" />
+                        <Skeleton className="h-4 w-[80%]" />
+                        <Skeleton className="h-4 w-[40%]" />
+                        <Skeleton className="h-4 w-[60%]" />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className="pb-40">
+            <Cover url={"/#"} preview={false} />
+            <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+                <Toolbar initialData={document} />
+                <Editor
+                    onChange={() => { }}
+                    initialContent={document}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;
